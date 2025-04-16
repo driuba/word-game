3321020430c7a595941642bccbdbdcbff138449b
Andrius Andrikonis (andrandr) <andrius.andrikonis@toughlex.com>
2025-04-16 16:36:15 +0300

    Regex fix for word matching

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -3,5 +3,5 @@ export const textReplacement = /\$\{([\w-]+)}/gi;
 export const wordValidationPattern = /^\p{L}+$/giu;
 
 export function wordGuessPattern(word: string) {
-  return new RegExp(String.raw`[\P{L}\P{N}]?${word}[\P{L}\P{N}]?`, 'giu');
+  return new RegExp(String.raw`(?<![\p{L}\p{N}])${word}(?![\p{L}\p{N}])`, 'giu');
 }
