33620b9f872a1f228a89db181d78a902e6ae5371
Andrius Andrikonis <andrius.andrikonis@toughlex.com>
2025-04-27 19:04:20 +0300

    Postgresql
    Migrated database to Postgresql

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -6,11 +6,15 @@ export * from './statistic';
 export * from './word';
 
 export default new DataSource({
-  database: 'local.sqlite3',
+  database: process.env.DB_DATABASE,
   entities: [
     Statistic,
     Word
   ],
+  host: process.env.DB_HOST,
   migrations: ['src/migrations/**/*'],
-  type: 'sqlite'
+  password: process.env.DB_PASSWORD,
+  schema: process.env.DB_SCHEMA,
+  username: process.env.DB_USERNAME,
+  type: 'postgres'
 });
--- src/entities/statistic.ts
+++ src/entities/statistic.ts
@@ -19,22 +19,22 @@ import { Word } from './word';
       .addCommonTableExpression(groupScores(dataSource, wordsGuessedAll), scoresAll)
       .addCommonTableExpression(groupScores(dataSource, wordsGuessedWeek), scoresWeek)
       .from(channelUsers, 'cu')
-      .select('cu.ChannelId')
-      .addSelect('cu.UserId')
-      .addSelect('COALESCE(sw.Score, 0)', 'ScoreWeek')
-      .addSelect('COALESCE(sw.Average, 0)', 'AverageWeek')
-      .addSelect('COALESCE(sw.Maximum, 0)', 'MaximumWeek')
-      .addSelect('COALESCE(gw.Guesses, 0)', 'GuessesWeek')
-      .addSelect('COALESCE(sa.Score, 0)', 'ScoreAll')
-      .addSelect('COALESCE(sa.Average, 0)', 'AverageAll')
-      .addSelect('COALESCE(sa.Maximum, 0)', 'MaximumAll')
-      .addSelect('COALESCE(ga.Guesses, 0)', 'GuessesAll')
-      .leftJoin(guessesAll, 'ga', 'ga.ChannelId = cu.ChannelId AND ga.UserId = cu.UserId')
-      .leftJoin(guessesWeek, 'gw', 'gw.ChannelId = cu.ChannelId AND gw.UserId = cu.UserId')
-      .leftJoin(scoresAll, 'sa', 'sa.ChannelId = cu.ChannelId AND sa.UserId = cu.UserId')
-      .leftJoin(scoresWeek, 'sw', 'sw.ChannelId = cu.ChannelId AND sw.UserId = cu.UserId')
-      .orderBy('sa.Score', 'DESC', 'NULLS LAST')
-      .addOrderBy('ga.Guesses', 'DESC', 'NULLS LAST');
+      .select('"cu"."ChannelId"')
+      .addSelect('"cu"."UserId"')
+      .addSelect('COALESCE("sw"."Score", 0)', 'ScoreWeek')
+      .addSelect('COALESCE("sw"."Average", 0)', 'AverageWeek')
+      .addSelect('COALESCE("sw"."Maximum", 0)', 'MaximumWeek')
+      .addSelect('COALESCE("gw"."Guesses", 0)', 'GuessesWeek')
+      .addSelect('COALESCE("sa"."Score", 0)', 'ScoreAll')
+      .addSelect('COALESCE("sa"."Average", 0)', 'AverageAll')
+      .addSelect('COALESCE("sa"."Maximum", 0)', 'MaximumAll')
+      .addSelect('COALESCE("ga"."Guesses", 0)', 'GuessesAll')
+      .leftJoin(guessesAll, 'ga', '"ga"."ChannelId" = "cu"."ChannelId" AND "ga"."UserId" = "cu"."UserId"')
+      .leftJoin(guessesWeek, 'gw', '"gw"."ChannelId" = "cu"."ChannelId" AND "gw"."UserId" = "cu"."UserId"')
+      .leftJoin(scoresAll, 'sa', '"sa"."ChannelId" = "cu"."ChannelId" AND "sa"."UserId" = "cu"."UserId"')
+      .leftJoin(scoresWeek, 'sw', '"sw"."ChannelId" = "cu"."ChannelId" AND "sw"."UserId" = "cu"."UserId"')
+      .orderBy('"sa"."Score"', 'DESC', 'NULLS LAST')
+      .addOrderBy('"ga"."Guesses"', 'DESC', 'NULLS LAST');
   },
   name: 'Statistics'
 })
@@ -82,39 +82,39 @@ const wordsGuessedWeek = 'WordsGuessedWeek';
 function groupGuesses(dataSource: DataSource, from: string) {
   return dataSource
     .createQueryBuilder()
-    .select('w.ChannelId')
-    .addSelect('w.UserIdGuesser', 'UserId')
+    .select('"w"."ChannelId"')
+    .addSelect('"w"."UserIdGuesser"', 'UserId')
     .addSelect('COUNT(1)', 'Guesses')
     .from(from, 'w')
-    .groupBy('w.ChannelId')
-    .addGroupBy('w.UserIdGuesser');
+    .groupBy('"w"."ChannelId"')
+    .addGroupBy('"w"."UserIdGuesser"');
 }
 
 function groupScores(dataSource: DataSource, from: string) {
   return dataSource
     .createQueryBuilder()
-    .select('w.ChannelId')
-    .addSelect('w.UserIdCreator', 'UserId')
-    .addSelect('SUM(w.Score)', 'Score')
-    .addSelect('AVG(w.Score)', 'Average')
-    .addSelect('MAX(w.Score)', 'Maximum')
+    .select('"w"."ChannelId"')
+    .addSelect('"w"."UserIdCreator"', 'UserId')
+    .addSelect('SUM("w"."Score")', 'Score')
+    .addSelect('AVG("w"."Score")', 'Average')
+    .addSelect('MAX("w"."Score")', 'Maximum')
     .from(from, 'w')
-    .groupBy('w.ChannelId')
-    .addGroupBy('w.UserIdCreator');
+    .groupBy('"w"."ChannelId"')
+    .addGroupBy('"w"."UserIdCreator"');
 }
 
 function selectChannelUsers(dataSource: DataSource) {
   const userIdsCreator = dataSource
     .createQueryBuilder()
-    .select('wga.ChannelId')
-    .addSelect('wga.UserIdCreator', 'UserId')
+    .select('"wga"."ChannelId"')
+    .addSelect('"wga"."UserIdCreator"', 'UserId')
     .from(wordsGuessedAll, 'wga')
     .getQuery();
 
   const userIdsGuesser = dataSource
     .createQueryBuilder()
-    .select('wga.ChannelId')
-    .addSelect('wga.UserIdGuesser', 'UserId')
+    .select('"wga"."ChannelId"')
+    .addSelect('"wga"."UserIdGuesser"', 'UserId')
     .from(wordsGuessedAll, 'wga')
     .getQuery();
 
@@ -125,12 +125,12 @@ function selectWordsGuessedAll(dataSource: DataSource) {
   return dataSource
     .createQueryBuilder()
     .from(Word, 'w')
-    .where('w.UserIdGuesser IS NOT NULL');
+    .where('"w"."UserIdGuesser" IS NOT NULL');
 }
 
 function selectWordsGuessedWeek(dataSource: DataSource) {
   return dataSource
     .createQueryBuilder()
     .from(wordsGuessedAll, 'wga')
-    .where(`wga.Created >= DATE('now', 'weekday 0', '-6 days')`);
+    .where(`"wga"."Created" >= date_trunc('week', CURRENT_TIMESTAMP)`);
 }
