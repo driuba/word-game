6b2ed90600aff84a6b836cfe3e8745a92f55d4db
Andrius Andrikonis <andrius.andrikonis@toughlex.com>
2025-07-21 10:15:15 +0300

    Added batch error handling to private report worker

--- src/workers/reportPrivate.ts
+++ src/workers/reportPrivate.ts
@@ -2,6 +2,7 @@ import type { App } from '@slack/bolt';
 import { DateTime } from 'luxon';
 import { getWordExpiration, getWordsActive } from '~/core/index.js';
 import { messages } from '~/resources/index.js';
+import { ApplicationError } from '~/utils/index.js';
 
 const dateToday = DateTime
 	.now()
@@ -17,7 +18,11 @@ export default async function (this: App) {
 		})
 		.then(r => new Set(r.channels?.map(c => c.id)));
 
+	let count = 0;
+
 	if (channelIds.size) {
+		const errors: unknown[] = [];
+
 		const words = await getWordsActive().then(
 			ws => ws.map(w => ({
 				channelId: w.channelId,
@@ -29,23 +34,33 @@ export default async function (this: App) {
 		);
 
 		for (const word of words) {
-			if (!channelIds.has(word.channelId)) {
-				continue;
-			}
+			try {
+				if (!channelIds.has(word.channelId)) {
+					continue;
+				}
 
-			if (!word.expiration || word.expiration.startOf('day') > dateToday) {
-				continue;
-			}
+				if (!word.expiration || word.expiration.startOf('day') > dateToday) {
+					continue;
+				}
 
-			await this.client.chat.postMessage({
-				channel: word.userId,
-				text: messages.reportPrivate({
-					...word,
-					expiration: word.expiration.toLocaleString(DateTime.DATETIME_SHORT)
-				})
-			});
+				await this.client.chat.postMessage({
+					channel: word.userId,
+					text: messages.reportPrivate({
+						...word,
+						expiration: word.expiration.toLocaleString(DateTime.DATETIME_SHORT)
+					})
+				});
+
+				count++;
+			} catch (error) {
+				errors.push(error);
+			}
+		}
+
+		if (errors.length) {
+			throw new ApplicationError('Failed to notify one or more users about expiration.', { errors });
 		}
 	}
 
-	this.logger.info('Finishing personal report.');
+	this.logger.info(`Finishing personal report, notified ${count.toFixed()} users.`);
 }
