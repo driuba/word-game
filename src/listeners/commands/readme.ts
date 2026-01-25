0782886e194c5ddf81d7ad0d749de57f32c49a6c
Andrius Andrikonis <andrikonis.andrius@gmail.com>
2026-01-25 16:19:49 +0200

    Simplified functions

--- src/listeners/commands/readme.ts
+++ src/listeners/commands/readme.ts
@@ -1,12 +1,12 @@
 import type { AllMiddlewareArgs, SlackCommandMiddlewareArgs } from '@slack/bolt';
 import { readme } from '~/resources/index.js';
 
-export default async function (
+export default function (
 	{
 		ack
 	}: AllMiddlewareArgs & SlackCommandMiddlewareArgs
 ) {
-	await ack({
+	return ack({
 		response_type: 'ephemeral',
 		text: readme
 	});
--- src/listeners/commands/version.ts
+++ src/listeners/commands/version.ts
@@ -2,12 +2,12 @@ import type { AllMiddlewareArgs, SlackCommandMiddlewareArgs } from '@slack/bolt'
 import config from '~/config.js';
 import { messages } from '~/resources/index.js';
 
-export default async function (
+export default function (
 	{
 		ack
 	}: AllMiddlewareArgs & SlackCommandMiddlewareArgs
 ) {
-	await ack({
+	return ack({
 		response_type: 'ephemeral',
 		text: messages.version({
 			version: config.version
